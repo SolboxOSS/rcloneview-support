7d0eb285bfb52c555c4f05938601991bae562d2c
jay <jay@solbox.com>
2025-07-08 13:05:27 +0900

    -side bar 스크롤링 제한 수정.

--- src/theme/DocSidebar/index.js
+++ src/theme/DocSidebar/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import DocSidebar from '@theme-original/DocSidebar';
 import CustomSearchBox from '@site/src/components/CustomSearchBox';
+import styles from './styles.module.css'; // 별도 스타일 분리 권장
 
 export default function DocSidebarWrapper({ query, setQuery, ...props }) {
   return (
-    <>
-      {/* 네비바에 가리지 않도록 paddingTop 추가 */}
-      <div style={{ paddingTop: '2rem', position: 'sticky', top: 0, zIndex:1000}}>
+    <div className={styles.sidebarContainer}>
+      <div className={styles.sidebarSearchBox}>
         <CustomSearchBox query={query} setQuery={setQuery} />
       </div>
-      {/* DocSidebar 자체에 marginTop이 있으면 0으로 조정 */}
-
-      <DocSidebar {...props }/>
-    </>
+      <div className={styles.sidebarMenu}>
+        <DocSidebar {...props} />
+      </div>
+    </div>
   );
 }
